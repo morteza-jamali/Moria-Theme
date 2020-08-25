f1b9bdd792345c82d2cabbf56c906bc017d71fa6
morteza-jamali <mortezajamali4241@gmail.com>
2020-08-24 20:11:58 -0400

    Index page updated #2

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,3 @@ import Icons from 'uikit/dist/js/uikit-icons';
 
 // loads the Icon plugin
 UIkit.use(Icons);
-
-// components can be called from the imported UIkit reference
-UIkit.notification('Hello world.');
